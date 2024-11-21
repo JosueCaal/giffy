0dde03fbf9d3188518ec2924e0b3f5ae7bce9091
Josue Caal <josuecaal787@gmail.com>
2024-11-21 00:36:18 -0600

    Agregar detail y rutas para gifs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import getGifs from './services/getGif';
-import Gif from './components/Gif';
-import ListOfGifs from './components/ListOfGifs';
 import Home from './pages/Homes/index.js';
 import { Route, Link } from 'wouter';
 import searchResults from './pages/SearchResults/index.js';
+import StaticContext from './context/StaticContext.js';
+import Detail from './pages/Detail/index.js';
 
 
 
 function App() {
 
   return (
-    <div className="App">
-      <section className="App-content">
-        <Link to="/">
-          <img className='App-logo' alt='giffy-logo' src='./logogiff.png'/>
-        </Link>
-        <Route 
-          component={Home}
-          path="/" />
-        <Route 
-          component={searchResults}
-          path="/search/:keyword" />
-        <Route 
-          //component={Detail}
-          path="/gif/:id" />
-      </section>
-    </div>
+
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <img className='App-logo' alt='giffy-logo' src='/logogiff.png'/>
+          </Link>
+          <Route 
+            component={Home}
+            path="/" />
+          <Route 
+            component={searchResults}
+            path="/search/:keyword" />
+          <Route 
+            component={Detail}
+            path="/gif/:id" />
+        </section>
+      </div>
   );
 }
 
--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,9 +3,9 @@ import './Gif.css'
 
 export default function Gif({title, id, url }){
     return(
-        <a href ={`#${id}`} className="Gif">
+        <a href ={`/gif/${id}`} className="Gif">
          <h4>{title}</h4>
-         <img alt={title} src={url}/>
+         <img loading="lazy" alt={title} src={url}/>
         </a>
     )
 }
